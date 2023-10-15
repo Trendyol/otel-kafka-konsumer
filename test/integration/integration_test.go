3d6855f932f3a96e03cf525de703c8b31bdee764
henesgokdag <henesgokdag@gmail.com>
2023-10-15 09:57:09 +0300

    integration test fix

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -2,7 +2,6 @@ package integration
 
 import (
 	"context"
-	"fmt"
 	otelkafkakonsumer "github.com/Trendyol/otel-kafka-konsumer"
 	"github.com/segmentio/kafka-go"
 	"github.com/stretchr/testify/assert"
@@ -19,12 +18,10 @@ import (
 )
 
 func getTracer() *trace.TracerProvider {
-
 	tp := trace.NewTracerProvider(
 		trace.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
-			semconv.ServiceName("trace-demo"),
-			attribute.String("environment", "prod"),
+			semconv.ServiceName("integration-test"),
 		)),
 	)
 
@@ -32,74 +29,6 @@ func getTracer() *trace.TracerProvider {
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 
 	return tp
-}
-
-func Test_Producer_And_Consumer_Message_Have_Same_Trace_Id(t *testing.T) {
-	tracer := getTracer()
-
-	segmentioProducer := &kafka.Writer{
-		Addr: kafka.TCP("localhost:29092"),
-	}
-
-	writer, err := otelkafkakonsumer.NewWriter(segmentioProducer,
-		otelkafkakonsumer.WithTracerProvider(tracer),
-		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
-		otelkafkakonsumer.WithAttributes(
-			[]attribute.KeyValue{
-				semconv.MessagingDestinationKindTopic,
-				semconv.MessagingKafkaClientIDKey.String("opentel-cg"),
-			},
-		))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer writer.Close()
-
-	message := kafka.Message{Topic: "opentel", Value: []byte("1")}
-
-	// Extract tracing info from message
-	ctx := writer.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(&message))
-
-	tr := tracer.Tracer("otel-kafka-konsumer")
-	parentCtx, producerSpan := tr.Start(ctx, "before produce operation")
-	time.Sleep(100 * time.Millisecond)
-	producerSpan.End()
-
-	writer.WriteMessage(parentCtx, message)
-
-	segmentioReader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:     []string{"localhost:29092"},
-		GroupTopics: []string{"opentel"},
-		GroupID:     "opentel-cg",
-	})
-
-	reader, err := otelkafkakonsumer.NewReader(
-		segmentioReader,
-		otelkafkakonsumer.WithTracerProvider(tracer),
-		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
-		otelkafkakonsumer.WithAttributes(
-			[]attribute.KeyValue{
-				semconv.MessagingDestinationKindTopic,
-				semconv.MessagingKafkaClientIDKey.String("opentel-cg"),
-			},
-		),
-	)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	consumerMessage, err := reader.ReadMessage(context.Background())
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	//assert
-	traceParent := strings.Split(string(consumerMessage.Headers[0].Value), "-")
-	println(producerSpan.SpanContext().TraceID().String())
-	println(traceParent[1])
-	assert.Equal(t, producerSpan.SpanContext().HasTraceID(), true)
-	assert.Equal(t, producerSpan.SpanContext().IsValid(), true)
-	assert.Equal(t, producerSpan.SpanContext().HasSpanID(), true)
-	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), traceParent[1])
 }
 
 func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
@@ -160,10 +89,6 @@ func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
 	consumerCtx := reader.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(consumerMessage))
 	consumerCtx, workSpan := tr.Start(consumerCtx, "work")
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
 	time.Sleep(100 * time.Millisecond)
 	workSpan.End()
 
@@ -172,13 +97,11 @@ func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
 	anotherWorkSpan.End()
 
 	//assert
-	println(producerSpan.SpanContext().TraceID().String())
-	println(anotherWorkSpan.SpanContext().TraceID().String())
-	println(workSpan.SpanContext().TraceID().String())
-
+	traceParent := strings.Split(string(consumerMessage.Headers[0].Value), "-")
 	assert.Equal(t, producerSpan.SpanContext().HasTraceID(), true)
 	assert.Equal(t, producerSpan.SpanContext().IsValid(), true)
 	assert.Equal(t, producerSpan.SpanContext().HasSpanID(), true)
 	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), anotherWorkSpan.SpanContext().TraceID().String())
 	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), workSpan.SpanContext().TraceID().String())
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), traceParent[1])
 }
