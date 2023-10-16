6d44e1ca69233b14067ad3f49416f7bede66d0fc
H.Enes Gökdağ <56965959+henesgokdag@users.noreply.github.com>
2023-10-16 23:05:38 +0300

    tests: integration tests added (#7)
    * integration tests added
    
    * integration test fix
    
    * dockerfile changed to redpanda
    
    * test: integration test seperated from unit tests

---
+++ test/integration/go.mod
@@ -0,0 +1,24 @@
+module integration
+
+go 1.19
+
+require (
+	github.com/Trendyol/otel-kafka-konsumer v0.0.5
+	github.com/segmentio/kafka-go v0.4.44
+	github.com/stretchr/testify v1.8.4
+	go.opentelemetry.io/otel v1.19.0
+	go.opentelemetry.io/otel/sdk v1.19.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.18 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel/metric v1.19.0 // indirect
+	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ test/integration/integration_test.go
@@ -0,0 +1,107 @@
+package integration
+
+import (
+	"context"
+	otelkafkakonsumer "github.com/Trendyol/otel-kafka-konsumer"
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"log"
+	"strings"
+	"testing"
+	"time"
+)
+
+func getTracer() *trace.TracerProvider {
+	tp := trace.NewTracerProvider(
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("integration-test"),
+		)),
+	)
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return tp
+}
+
+func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
+	tracer := getTracer()
+	segmentioProducer := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		AllowAutoTopicCreation: true,
+	}
+	writer, err := otelkafkakonsumer.NewWriter(segmentioProducer,
+		otelkafkakonsumer.WithTracerProvider(tracer),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("opentel-cg"),
+			},
+		))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer writer.Close()
+
+	message := kafka.Message{Topic: "opentel-consumer-test", Value: []byte("1")}
+
+	// Extract tracing info from message
+	ctx := writer.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(&message))
+
+	tr := tracer.Tracer("otel-kafka-konsumer")
+	parentCtx, producerSpan := tr.Start(ctx, "before produce operation")
+	time.Sleep(100 * time.Millisecond)
+	producerSpan.End()
+
+	writer.WriteMessage(parentCtx, message)
+
+	segmentioReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"localhost:9092"},
+		GroupTopics: []string{"opentel-consumer-test"},
+		GroupID:     "opentel-cg",
+	})
+
+	reader, err := otelkafkakonsumer.NewReader(
+		segmentioReader,
+		otelkafkakonsumer.WithTracerProvider(tracer),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("opentel-cg"),
+			},
+		),
+	)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	consumerMessage, err := reader.ReadMessage(context.Background())
+
+	// Extract tracing info from message
+	consumerCtx := reader.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(consumerMessage))
+	consumerCtx, workSpan := tr.Start(consumerCtx, "work")
+
+	time.Sleep(100 * time.Millisecond)
+	workSpan.End()
+
+	_, anotherWorkSpan := tr.Start(consumerCtx, "another work")
+	time.Sleep(50 * time.Millisecond)
+	anotherWorkSpan.End()
+
+	//assert
+	traceParent := strings.Split(string(consumerMessage.Headers[0].Value), "-")
+	assert.Equal(t, producerSpan.SpanContext().HasTraceID(), true)
+	assert.Equal(t, producerSpan.SpanContext().IsValid(), true)
+	assert.Equal(t, producerSpan.SpanContext().HasSpanID(), true)
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), anotherWorkSpan.SpanContext().TraceID().String())
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), workSpan.SpanContext().TraceID().String())
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), traceParent[1])
+}
