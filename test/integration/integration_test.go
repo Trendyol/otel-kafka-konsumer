729076ffd32a565f3b6970e8da59262a6947a6a0
Abdulsametileri <sametileri07@gmail.com>
2023-12-07 08:29:10 +0300

    feat: add integration test

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -31,7 +31,7 @@ func getTracer() *trace.TracerProvider {
 	return tp
 }
 
-func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
+func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id_In_AutoCommit_Mode(t *testing.T) {
 	tracer := getTracer()
 	segmentioProducer := &kafka.Writer{
 		Addr:                   kafka.TCP("localhost:9092"),
@@ -95,6 +95,83 @@ func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id(t *testing.T) {
 	_, anotherWorkSpan := tr.Start(consumerCtx, "another work")
 	time.Sleep(50 * time.Millisecond)
 	anotherWorkSpan.End()
+
+	//assert
+	traceParent := strings.Split(string(consumerMessage.Headers[0].Value), "-")
+	assert.Equal(t, producerSpan.SpanContext().HasTraceID(), true)
+	assert.Equal(t, producerSpan.SpanContext().IsValid(), true)
+	assert.Equal(t, producerSpan.SpanContext().HasSpanID(), true)
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), anotherWorkSpan.SpanContext().TraceID().String())
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), workSpan.SpanContext().TraceID().String())
+	assert.Equal(t, producerSpan.SpanContext().TraceID().String(), traceParent[1])
+}
+
+func Test_Producer_And_Consumer_Spans_Have_Same_Trace_Id_In_ManualCommit_Mode(t *testing.T) {
+	tracer := getTracer()
+	segmentioProducer := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		AllowAutoTopicCreation: true,
+	}
+	writer, err := otelkafkakonsumer.NewWriter(segmentioProducer,
+		otelkafkakonsumer.WithTracerProvider(tracer),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("opentel-cg-manual-commit"),
+			},
+		))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer writer.Close()
+
+	message := kafka.Message{Topic: "opentel-consumer-test-manual-commit", Value: []byte("1")}
+
+	// Extract tracing info from message
+	ctx := writer.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(&message))
+
+	tr := tracer.Tracer("otel-kafka-konsumer")
+	parentCtx, producerSpan := tr.Start(ctx, "before produce operation")
+	time.Sleep(100 * time.Millisecond)
+	producerSpan.End()
+
+	writer.WriteMessage(parentCtx, message)
+
+	segmentioReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"localhost:9092"},
+		GroupTopics: []string{"opentel-consumer-test-manual-commit"},
+		GroupID:     "opentel-cg-manual-commit",
+	})
+
+	reader, err := otelkafkakonsumer.NewReader(
+		segmentioReader,
+		otelkafkakonsumer.WithTracerProvider(tracer),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("opentel-cg-manual-commit"),
+			},
+		),
+	)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	consumerMessage, err := reader.FetchMessage(context.Background())
+
+	// Extract tracing info from message
+	consumerCtx := reader.TraceConfig.Propagator.Extract(context.Background(), otelkafkakonsumer.NewMessageCarrier(consumerMessage))
+	consumerCtx, workSpan := tr.Start(consumerCtx, "work")
+
+	time.Sleep(100 * time.Millisecond)
+	workSpan.End()
+
+	_, anotherWorkSpan := tr.Start(consumerCtx, "another work")
+	time.Sleep(50 * time.Millisecond)
+	anotherWorkSpan.End()
+
+	reader.CommitMessages(context.Background(), *consumerMessage)
 
 	//assert
 	traceParent := strings.Split(string(consumerMessage.Headers[0].Value), "-")
